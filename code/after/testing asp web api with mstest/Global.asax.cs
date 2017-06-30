f830ce27a4fb067e0ca37ebe99608d0a337fbe70
tshepontlhokoa <tshepo.ntlhokoa@gmail.com>
2017-06-30 06:04:57 +0200

    first message

---
+++ code/after/testing asp web api with mstest/Global.asax.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace testing_asp_web_api_mstest
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
---
+++ code/after/web api tests/RootControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using testing_asp_web_api_mstest.Controllers;
+
+namespace web_api_tests
+{
+    [TestClass]
+    public class RootControllerTests
+    {
+        [TestMethod]
+        public void RootController_Get_WillReturnString()
+        {
+            var controller = new RootController();
+            var message = controller.Get();
+            Assert.AreEqual("API is ready to receive requests", message);
+        }
+    }
+}
+
+
+
